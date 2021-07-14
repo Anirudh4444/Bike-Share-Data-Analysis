d08e5909094a7879aeeba2737d8a383de34b4b33
Anirudh4444 <agulabk4444@gmail.com>
2021-07-14 16:10:30 +0530

    Delete dece.py

--- dece.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-
-
-def decryption(unque_id,password):   
-    unque_id=unque_id.encode()
-    password=password.encode()
-    f=Fernet(unque_id)
-    decrypted=f.decrypt(password).decode()
-    return decrypted
